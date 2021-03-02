61e9a44419c48bbccfe1386dfcc1b00d37dcce87
zhaoliheng <527097913@qq.com>
2021-03-02 17:48:34 +0800

    配置文件

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button @click="state.count++">count is: {{ state.count }} {{state2.count}} {{state.count2}}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+
+  <div class="main">
+     <router-link to='/home'> Home</router-link>
+     <br>
+     <br>
+
+     <router-link to='/details'> details</router-link>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 2,count2: 2 })
+const state2 = reactive({ count: 23 })
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: #42b983;
+}
+.main {
+  height: 100px;
+}
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>1111dwqdqw1d</div>
+  <div class='form-element'>
+    <h2> {{ state.title }} </h2>
+    <input type='text' v-model='state.username' placeholder='Username' />
+
+    <input type='password' v-model='state.password' placeholder='Password' />
+
+    <button @click='login'>
+      Submit
+    </button>
+    <p>
+      Values: {{ state.username + ' ' + state.password }}
+    </p>
+    <p> lowerCaseUsername : {{lowerCaseUsername}}</p>
+  </div>
+</template>
+<script>
+import { reactive, onMounted, computed } from 'vue';
+
+export default {
+  props: {
+    title: String
+  },
+
+  emits: ['login'],
+  setup(props, { emit }) {
+    const state = reactive({
+      username: '',
+      password: ''
+    });
+    const lowerCaseUsername = computed(() => state.username.toLowerCase());
+    onMounted(() => {
+      console.log('组件已挂载');
+      console.log('title: ' + props.title);
+      console.log('title: ' + state.lowerCaseUsername);
+    });
+
+    const login = () => {
+      emit('login', {
+        username: state.username,
+        password: state.password
+      });
+    };
+    return {
+      login,
+      lowerCaseUsername,
+      state
+    };
+  }
+};
+</script>
